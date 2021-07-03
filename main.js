1a78dc3c25ee6a8ac08747147de0cafbcddb6d54
李业宏扬 <845206213@qq.com>
2021-07-03 15:15:21 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ import App from './App';
 import uView from 'uview-ui';
 import httpInterceptor from '@/common/http.interceptor.js';
 import httpApi from '@/common/http.api.js';
+import store from '@/store';
+const vuexStore = require('@/store/$u.mixin.js');
 
 Object.values = (obj) => {
 	var arr = [];
@@ -18,11 +20,13 @@ Object.keys = (obj) => {
 }
 
 Vue.config.productionTip = false
+Vue.mixin(vuexStore);
 App.mpType = 'app'
 
 Vue.use(uView);
 
 const app = new Vue({
+	store,
 	...App
 })
 Vue.use(httpInterceptor, app);
@@ -42,4 +46,58 @@ Vue.prototype.alert = (msg) => {
 		});
 	});
 }
+Vue.prototype.$wx = {
+	login() {
+		return new Promise((recv, recj) => {
+			uni.login({
+				success: (res) => {
+					this.$u.api.user.login({
+						code: res.code
+					}).then((e) => {
+						this.$wx.setLoginData(e,recv,recj);
+					});
+				},
+				fail: recj
+			});
+		});
+	},
+	register() {
+		return new Promise((recv, recj) => {
+			uni.getUserProfile({
+				desc: '登录授权',
+				success(userInfo) {
+					uni.login({
+						success(res) {
+							return recv({
+								code: res.code,
+								userInfo: userInfo
+							});
+						},
+						fail: recj
+					});
+				},
+				fail: recj
+			});
+		}).then((e) => {
+			return Vue.prototype.$u.api.user.register({
+				data: JSON.stringify(e)
+			});
+		});
+	},
+	setLoginData(loginPromiseReturnVal,recv,recj) {
+		var e = loginPromiseReturnVal;
+		if (e.status) {
+			uni.setStorageSync("userToken", e.data.token);
+			Vue.prototype.$u.vuex("userInfo", e.data.user);
+			recv(e.data.user);
+		} else{
+			this.removeLoginData();
+			recj(e.msg)
+		}
+	},
+	removeLoginData() {
+		uni.removeStorageSync("userToken");
+		Vue.prototype.$u.vuex("userInfo", null);
+	}
+};
 app.$mount()
